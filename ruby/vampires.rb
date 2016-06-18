0e175ea3163ab372609fa93163ee63c0c0d46667
elillian <elillian@gmail.com>
2016-06-17 19:59:01 -0700

    added info to vamppires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,43 @@
+
+New Employee Vampire Detecting Interview
+
+What is your name?
+puts "Name is #{name_in_questions}"
+any name = probably human, honestly
+
+How old are you? What year were you born?
+200-900+ years old? definitely a Vampire!
+1-100 human
+100+ = possible vampire, or just really healthy
+
+Our company cafeteria serves garlic bread. Should we order some for you?
+yes = human
+no= possible vampire
+maybe, n/a = possible vampire
+
+Would you like to enroll in the company's health insurance?
+yes = human
+no = possible vampire
+maybe, n/a = possible vampire
+
+---------------------
+
+Results:
+
+Probably not a vampire.
+
+Probably a vampire.
+
+Almost certainly a vampire.
+
+Definitely a vampire.
+
+Results inconclusive.
+
+----------------------------
+
+Message at the end:
+
+Actually, never mind! 
+What do these questions have to do with anything? 
+Let's all be friends.
