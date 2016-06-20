7f78211186f11556eb87c00d0d6b5e6f75e0c6c6
Jacqueline Shaffer <river16.dbc@gmail.com>
2016-06-19 22:38:37 -0400

    Week 4 assignments 42 43 44rv1

---
+++ ruby/vampires.rb
@@ -0,0 +1,36 @@
+#Ask how many ees will be processed
+# get count
+# loop survey for that count
+
+#Ask employee they have allergies
+#Name allergy one at a time
+#
+#
+puts "Do you have any allergies?  yes or no"
+  allergy = gets.chomp
+if allergy = "no"
+  said_y = false
+elsif allergy = "yes"
+  said_n = true
+
+
+
+
+  If no,
+    end
+  If yes,
+    do you have more than one allergy?  yes or no
+    If no,
+    Ask to name 1
+      if alergy name = sunshine
+        puts probably a vampire
+      else end
+    If yes,
+      loop to continue asking for allergies
+      Ask if have anymore after each on??
+      Do you have anymore allergies?  Yes / Nor
+      If yes, continue loop
+      If no, stop/end
+    Check along the way if allergy = sunshine
+
+
