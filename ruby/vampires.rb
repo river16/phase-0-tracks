eb4306849346d9bb632d676a839ea4e23bdac93e
elillian <elillian@gmail.com>
2016-06-20 06:03:16 -0700

    added loop to vampire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,6 +1,9 @@
 
 #New Employee Vampire Detecting Interview
 
+times_to_loop = 0
+	loop do
+
 #What is your name?
 puts "What is your name?"
 employee_name = gets.chomp
@@ -14,18 +17,39 @@ puts "#{employee_age}"
 #What year were you born?
 puts "What year were you born?"
 employee_birth_year = gets.chomp.to_i
-1900-2016 = human
-1700-1900 = vampire 
+1900-2016 = human_age
+1700-1900 = vampire_age
+
+#age_test
+
+if age_test == (2016- employee_birth_year)
+	1900-2016 = human_age
+	1700-1900 = vampire_age
+			age_test = true
+		else
+			age_test = false
+		end
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? 
 y/n)"
-y = human
-n= possible vampire
+y = wants_garlic
+n = no_garlic
 
 #Would you like to enroll in the company's health insurance?
 puts "Would you like to enroll in the company's health insurance? (yes/no)"
-yes = human
-no = possible vampire
+yes = wants_health_ins
+no = no_health_ins
+
+if age_test && (wants_garlic || wants_health_ins) == true
+			puts "Probably not a vampire."
+elsif age_test == false && (wants_garlic || wants_health_ins) == true
+			puts "Probably a vampire."
+elsif age_test || wants_garlic || wants_health_ins == false
+			puts "Probably a vampire."
+			else
+			puts "Who really knows? Let's just ask the source ;)"
+		end
+loop do
 
 #Message at the end:
 
