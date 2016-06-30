4517bb8d0afdab09bfecf07e498a25a985d4bbc1
elillian <elillian@gmail.com>
2016-06-30 16:38:21 -0700

    added methods for puppy

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,70 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
-
+  
+  def speak(number)
+	number.times{puts "Woof!"}
+	end
+	
+	def roll_over
+		puts "*roll over*"
+	end
+	
+	def dog_years(years)
+		puts "I am #{years*7}  human years old"
+	end
+	
+	def sit
+	puts "I just sat!"
+	end
+	
+	def initialize
+	puts "Initializing new puppy instance ..."
+	end
+		
 end
+
+thomas = Puppy.new
+thomas.fetch("ball")
+thomas.speak(9)
+thomas.roll_over
+thomas.dog_years(6)
+thomas.sit
+
+class Cat
+	def meow(mew)
+	mew.times{puts "Meow!"}
+	end
+	
+	def scratch
+	puts "I like to scratch furniture!"
+	end
+	
+	def initialize
+	puts "Initializing new cat instance"
+	end
+	
+end
+
+felix = Cat.new
+felix.meow(3)
+felix.scratch
+
+pound = []
+ ctr = 0
+ 
+ while ctr < 50
+   pound << Cat.new
+   ctr += 1
+ end
+ 
+ puts pound.length
+ 
+ pound.each_with_index do |cat, index|
+   pound.scratch
+   pound.scratch
+   pound.meow(5)
+   pound.meow(1)
+ end
+ 
 
