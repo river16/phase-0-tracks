70e237165215cd85632b0dcce6c4338195591ebe
elillian <elillian@gmail.com>
2016-06-30 16:32:42 -0700

    added methods to do list

--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,21 +1,29 @@
+require_relative 'todo_list'
 describe TodoList do
-  let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
 
-  it "stores the list items given on initialization" do
-    expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
-  end
+# let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
 
-  it "adds an item to the list" do
-    list.add_item("mop")
-    expect(list.get_items).to eq ["do the dishes", "mow the lawn", "mop"]
-  end
+ it "stores the list items given on initialization" do
+   list=TodoList.new
+   list.get_items(["do the dishes", "mow the lawn"])
+   expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
+ end
 
-  it "deletes an item" do
-    list.delete_item("do the dishes")
-    expect(list.get_items).to eq ["mow the lawn"]
-  end
+ it "adds an item to the list" do
+   list=TodoList.new
+   list.add_item(" mop ")
+   expect(list.get_items).to eq ["do the dishes", "mow the lawn", "mop"]
+ end
 
-  it "retrieves an item by index" do
-    expect(list.get_item(0)).to eq "do the dishes"
-  end
+ it "deletes an item" do
+   list=TodoList.new
+   list.delete_item("do the dishes")
+   expect(list.get_items).to eq ["mow the lawn"]
+ end
+
+ it "retrieves an item by index" do
+   list=TodoList.new
+   list.get_item(["do the dishes", "mow the lawn"])
+   expect(list.get_item(0)).to eq "do the dishes"
+ end
 end
