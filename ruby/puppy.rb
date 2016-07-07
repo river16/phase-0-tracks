f617ab115e9bd3df51c0d3d6558d949b74dc914a
elillian <elillian@gmail.com>
2016-07-07 16:49:20 -0700

    added puppy class and instance

---
+++ ruby/puppy.rb
@@ -0,0 +1,30 @@
+#Species
+#Puppiesarr Adorableus
+#Characteristics
+#Friendly: yes
+#eye count: 2
+#name: varies
+#color: varies
+#Behavior
+#snuggle
+#wiggle
+#smile
+
+class Puppy
+end
+
+Puppy.methods
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+spot.class
+duchess == fido
+fido.instance_of?(Array)
+fido.instance_of?(Puppy)
+spot.play_dead
+some_puppy_instance.bark
+
+Puppy.new
+some_puppy_instance.bark
