7406cd93727f6e19a47247117d99c572046a0752
River <river16.dbc@gmail.com>
2016-07-27 05:01:59 +0000

    6.1 Puppy initial commit

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+'''
+SPECIES ------------------------
+Canine
+
+CHARACTERISTICS ----------------
+Name: varies
+Breed: retriever
+Color: varies
+Webbed toes: yes
+Friendly: yes
+Likes
+
+
+BEHAVIOR -----------------------
+Jump
+Hug
+Scamper
+Nap
+Play
+'''
+
